24f787f06d301f562e7b4b5cebe443a17748286f
JonathanRLange <33233620+JonathanRLange@users.noreply.github.com>
2020-03-17 19:16:40 -0500

    GitHub pages (#3)
    * included build files
    
    * changed relative route

---
+++ docs/precache-manifest.8ff52ffdf5b0c6c0d1433e602a7b90f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4dda6a5e0ff3f99f291cbbea55870b",
+    "url": "/cube-pack-calculator/index.html"
+  },
+  {
+    "revision": "0ce58bce0bc228dbbdf7",
+    "url": "/cube-pack-calculator/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "8116152e22f2e07a8dd5",
+    "url": "/cube-pack-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ce58bce0bc228dbbdf7",
+    "url": "/cube-pack-calculator/static/js/2.b6b5b516.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/cube-pack-calculator/static/js/2.b6b5b516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8116152e22f2e07a8dd5",
+    "url": "/cube-pack-calculator/static/js/main.2bfd6224.chunk.js"
+  },
+  {
+    "revision": "ac60f651688ea4b548ab",
+    "url": "/cube-pack-calculator/static/js/runtime-main.7081fe15.js"
+  }
+]);
